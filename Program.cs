6fd7eac0db6993cb038a3a61d2c29ff27a9012ee
Yt1m <ytimrdonqaz@gmail.com>
2023-10-01 16:30:56 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Для проверки введите x: ");
+int x = int.Parse(Console.ReadLine());
+int x1 = (x / 10 % 100);
+int x2 = (x % 10);
+int x3 = (x2 * 100 + x1);
+Console.WriteLine($"число x = {x3}");
